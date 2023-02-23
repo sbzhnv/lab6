56d89390e7b689ff581f921d3d07c5a1b18c26c3
Guldana <122424443+sbzhnv@users.noreply.github.com>
2023-02-23 06:21:43 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 function greetings(name){
     var name = prompt("Please, enter your name:");
-    console.log("Hi, "+ name+”!”);
+    console.log("Hi, "+ name+"!");
+}
+
+function greatest_number(number_1, number_2){
+    var number_1 = prompt("Please, enter the first number:")
+    var number_2 = prompt("Please, enter the second number:")
+    if (number_1 > number_2){
+        console.log(number_1 + " is greater than " + number_2)
     }
+    else if (number_2 > number_1){
+        console.log(number_2 + " is greater than " + number_1)
+    }
+    else {
+        console.log("Numbers are equal")
+    }
+    let sum = (number_1 ** 2) + (number_2 ** 2);
+    console.log("Sum of squares of " + number_1 + " and " + number_2 + " is " + sum)
+}
